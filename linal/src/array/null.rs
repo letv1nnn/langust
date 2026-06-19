c2ac32bcda22fb629677ff6d3f2f926006f8f780
Artem Lytvyn <letv1n2007@icloud.com>
2026-06-20 00:47:23 +0100

    extended null buffer and added more tests

--- linal/src/array/null.rs
+++ linal/src/array/null.rs
@@ -1,5 +1,7 @@
 // bit 1 = null, bit 0 = valid
 
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct NullBuffer {
     bits: Vec<u8>,
@@ -13,8 +15,19 @@ impl NullBuffer {
             len: 0usize,
         }
     }
+    pub fn with_len(len: usize) -> Self {
+        Self {
+            bits: vec![0u8; len.div_ceil(len)],
+            len,
+        }
+    }
+    #[inline]
     pub const fn len(&self) -> usize {
         self.len
+    }
+    #[inline]
+    pub const fn is_empty(&self) -> bool {
+        self.len == 0usize
     }
     pub fn count_nulls(&self) -> usize {
         if self.bits.is_empty() {
@@ -37,16 +50,19 @@ impl NullBuffer {
     pub fn any_null(&self) -> bool {
         self.bits.iter().any(|b| *b != 0u8)
     }
+    #[inline]
     pub fn is_null(&self, idx: usize) -> bool {
         assert!(idx < self.len);
         let (byte, bit) = (idx / 8usize, idx % 8usize);
         (self.bits[byte] & (1u8 << bit)) != 0u8
     }
+    #[inline]
     pub fn set_null(&mut self, idx: usize) {
         assert!(idx < self.len);
         let (byte, bit) = (idx / 8usize, idx % 8usize);
         self.bits[byte] |= 1u8 << bit;
     }
+    #[inline]
     pub fn set_valid(&mut self, idx: usize) {
         assert!(idx < self.len);
         let (byte, bit) = (idx / 8usize, idx % 8usize);
@@ -86,6 +102,22 @@ impl From<Vec<u8>> for NullBuffer {
     }
 }
 
+// impl FromIterator<bool> for NullBuffer { 
+//     fn from_iter<T: IntoIterator<Item = bool>>(iter: T) -> Self {
+//         let values: Vec<bool> = iter.into_iter().collect();
+//         values.into()
+//     }
+// }
+
+impl Display for NullBuffer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for i in 0..self.len {
+            writeln!(f, "{}", if self.is_null(i) { "NaN" } else { "Value" })?;
+        }
+        Ok(())
+    }
+}
+
 // `cargo test -- --nocapture` to see the debug output
 #[cfg(test)]
 mod nullbuffer_array_tests {
@@ -95,22 +127,56 @@ mod nullbuffer_array_tests {
     fn contruction_from_vector() {
         let nb = NullBuffer::from(vec![true, false, true, false]);
         println!("{:08b}", &nb.bits[0]);
+        // count null/NaN values
         assert_eq!(nb.count_nulls(), 2usize);
+        // check each value
         assert!(nb.is_null(0usize));
         assert_eq!(nb.is_null(1usize), false);
         assert!(nb.is_null(2usize));
         assert_eq!(nb.is_null(3usize), false);
+        // check length
+        assert_eq!(nb.len(), 4usize);
+        // any nulls
+        assert!(nb.any_null());
 
         let nb = NullBuffer::from(vec![0u8, 1u8, 0u8, 1u8, 1u8]);
         println!("{:08b}", &nb.bits[0]);
+        // count null/NaN values
         assert_eq!(nb.count_nulls(), 2usize);
-        assert!(nb.is_null(0));
-        assert_eq!(nb.is_null(1), false);
-        assert!(nb.is_null(2));
-        assert_eq!(nb.is_null(3), false);
-        assert_eq!(nb.is_null(4), false);
+        // check each value
+        assert!(nb.is_null(0usize));
+        assert_eq!(nb.is_null(1usize), false);
+        assert!(nb.is_null(2usize));
+        assert_eq!(nb.is_null(3usize), false);
+        assert_eq!(nb.is_null(4usize), false);
+        // check length
+        assert_eq!(nb.len(), 5usize);
+        // any nulls
+        assert!(nb.any_null());
 
         let nb = NullBuffer::from(vec![0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 1u8, 0u8]);
         println!("{:08b} {:08b}", &nb.bits[0], &nb.bits[1]);
+        // check each value
+        for bit in 0..8usize {
+            assert!(nb.is_null(bit));
+        }
+        assert!(!nb.is_null(8usize));
+        assert!(nb.is_null(9usize));
+        // check length
+        assert_eq!(nb.len(), 10usize);
+        // any nulls
+        assert!(nb.any_null());
+    }
+
+    #[test]
+    fn setting_values() {
+        let mut nb = NullBuffer::from(vec![0u8]);
+        assert!(nb.is_null(0usize));
+        
+        nb.set_valid(0usize);
+        assert!(!nb.is_null(0usize));
+
+        nb.set_null(0usize);
+        assert!(nb.is_null(0usize));
     }
 }
